9c010867ea58a9eeee5abd9cf8c3b69968418c18
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-06-06 04:18:51 +0600

    vklnkl

--- main.dart
+++ main.dart
@@ -1,8 +1,18 @@
-void main() {
+/*void main() {
   print('Hello, Dart!');
 }
 
 
 main() {
   print('Hello, Dart!');
+}
+
+// 
+*/
+
+main(){
+  var firstname='shovo';
+
+  String lastname='xhode';
+  print('My name is $firstname $lastname');
 }
