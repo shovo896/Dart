764047e3c378eba63f33627929a517a928967e43
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-06-07 13:58:43 +0600

    dm,

--- main.dart
+++ main.dart
@@ -454,7 +454,16 @@ void main(){
 */ 
 
 
-
+// list 
+void main(){
+  var numbers = [1,2,3,4,5];
+  print(numbers);
+  print(numbers[0]);
+  print(numbers[1]);
+  print(numbers[2]);
+  print(numbers[3]);
+  print(numbers[4]);
+}
 
 
 
