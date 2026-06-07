b6822f221cf5289013f3c87913ee8e9312207c21
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-06-07 11:19:23 +0600

    jvnjk

--- main.dart
+++ main.dart
@@ -319,7 +319,7 @@ void main(){
   }
 }
 
-*/ 
+
 void main(){
 
   int number = 100 ;
@@ -340,7 +340,20 @@ void main(){
 
 
 }
-
+*/
+void main(){
+  int number = 101 ;
+  if (number %2 ==0){
+    print('Even');
+  } 
+  else if (number % 3 ==0){
+    print('Odd');
+  }
+  else {
+    print('confused');
+  
+  }
+}
 
 
 
