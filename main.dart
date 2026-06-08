6b46990eb9dd6f6cd8cdcf75f19a495f95465bc6
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-06-08 14:42:36 +0600

    vijhnj

--- main.dart
+++ main.dart
@@ -624,12 +624,16 @@ void main(){
 dynamic sum(var a, var b){
   return a + b ;
 }
-*/ 
+
 void main(){
   print(sum(10));
   print(sum(10,10));
 }
 dynamic sum(var num1, [var num2]) => num1 + (num2 ?? 0);
+
+*/ 
+
+
 
 
 
--- tempCodeRunnerFile.dart
+++ tempCodeRunnerFile.dart
@@ -1,1 +1,5 @@
-
+void main(){
+  print(sum(10));
+  print(sum(10,10));
+}
+dynamic sum(var num1, [var num2]) => num1 + (num2 ?? 0);
