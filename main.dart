4a7237e38b6c385cf5e6fab742110a7033c3f109
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-06-08 15:06:44 +0600

    ikjio;

--- main.dart
+++ main.dart
@@ -684,6 +684,28 @@ main(){
 */ 
 
 
+class Rectangle{
+  num left, top, width, height ;
+  Rectangle(this.left, this.top, this.width, this.height);
+
+  num get right => left + width ;
+  set right(num value) => left = value - width ;
+  num get bottom => top + height ;
+  set bottom(num value) => top = value - height ;
+
+}
+
+void main(){
+  var rect = Rectangle(3, 4, 20, 15);
+  print('left: ${rect.left}, top: ${rect.top}, right: ${rect.right}, bottom: ${rect.bottom}');
+  rect.right = 10;
+  print('left: ${rect.left}, top: ${rect.top}, right: ${rect.right}, bottom: ${rect.bottom}');
+}
+
+
+
+
+
 
 
 
